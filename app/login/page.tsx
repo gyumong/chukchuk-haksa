da05f3abf8042053ab0b8b13458ceb63fe4ffbc6
gyumong <rlaalsrb3559@naver.com>
2024-11-23 15:21:39 +0900

    format

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,18 +1,16 @@
 'use client';
 
 import React from 'react';
-
 import { kakaoLogin } from '@/lib/auth';
 
 export default function LoginPage() {
-
   const handleLogin = async () => {
     try {
       await kakaoLogin();
     } catch (error) {
       console.error('로그인 실패:', error);
       alert('로그인 중 문제가 발생했습니다.');
-    } 
+    }
   };
 
   return (
