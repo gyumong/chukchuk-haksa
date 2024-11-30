5852c9b789297aed15b8166dada9a311f80dd237
gyumong <rlaalsrb3559@naver.com>
2024-11-30 16:49:41 +0900

    feat: csrf, replay attack 방지 state, nonce 처리 로직 추가

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,4 +1,6 @@
+import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
+import { getCookie } from 'cookies-next';
 import { getKakaoToken } from '@/lib/auth/kakao';
 import { signInWithSupabase } from '@/lib/auth/supabase';
 import { AuthError } from '@/lib/error';
@@ -9,11 +11,21 @@ export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const next = searchParams.get('next') ?? `${origin}`;
   const code = searchParams.get('code');
+  const returnedState = searchParams.get('state');
 
-  if (!code) {
+  if (!code || !returnedState) {
     return NextResponse.redirect(`${origin}/login`);
   }
   try {
+    /**
+     * example: https://www.npmjs.com/package/cookies-next
+     */
+    const state = await getCookie('state', { cookies });
+
+    if (!state || state !== returnedState) {
+      throw new AuthError('Invalid state parameter.');
+    }
+
     const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? origin;
     const redirectUri = `${baseUrl}/auth/callback`;
 
@@ -22,6 +34,7 @@ export async function GET(request: Request) {
 
     return NextResponse.redirect(next);
   } catch (error) {
+    console.log('error', error);
     const errorCode = error instanceof AuthError ? error.message : 'UNKNOWN_ERROR';
     return NextResponse.redirect(`${origin}/login?error=${errorCode}`);
   }
