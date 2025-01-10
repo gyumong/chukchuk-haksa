2b1b8cffb34a136d86315ede14b6fcdfde8c7b22
gyumong <rlaalsrb3559@naver.com>
2025-01-10 14:35:48 +0900

    feat: session 유지를 위한 설정

---
+++ lib/auth/session.ts
@@ -0,0 +1,20 @@
+import { SessionOptions } from 'iron-session';
+
+export interface SessionData { 
+    username: string;
+    password:string;
+}
+
+export const sessionOptions: SessionOptions = {
+  cookieName: 'session',
+  password: process.env.SESSION_SECRET!,
+  cookieOptions: {
+    secure: process.env.NODE_ENV === 'production',
+    maxAge: 600, // 10분
+  },
+};
+
+export const defaultSession: SessionData = {
+    username: '',
+    password: '',
+};
