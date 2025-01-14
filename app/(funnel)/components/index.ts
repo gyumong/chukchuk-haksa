6a72556003f5f13aee273e00ab8bd3844818afd1
gyumong <rlaalsrb3559@naver.com>
2025-01-14 17:47:32 +0900

    feat: 약관동의 컴포넌트 추가

--- app/(funnel)/components/index.ts
+++ app/(funnel)/components/index.ts
@@ -1,2 +1,3 @@
 export { default as SchoolCard } from './SchoolCard/SchoolCard';
 export { default as FunnelHeadline } from './FunnelHeadline/FunnelHeadline';
+export { default as AgreementItem } from './AgreementItem/AgreementItem';
