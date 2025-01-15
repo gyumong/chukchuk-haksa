5eb376a0dd75e5451e85bf656f29297034c6b88d
gyumong <rlaalsrb3559@naver.com>
2025-01-15 12:26:23 +0900

    feat: target score page 추가

--- app/(funnel)/components/index.ts
+++ app/(funnel)/components/index.ts
@@ -2,3 +2,4 @@ export { default as SchoolCard } from './SchoolCard/SchoolCard';
 export { default as FunnelHeadline } from './FunnelHeadline/FunnelHeadline';
 export { default as AgreementItem } from './AgreementItem/AgreementItem';
 export { default as PrivacyPolicySheet } from './PrivacyPolicySheet/PrivacyPolicySheet';
+export { default as ScoreInput } from './ScoreInput/ScoreInput';
