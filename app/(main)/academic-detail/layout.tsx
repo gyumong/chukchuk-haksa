d38808347089831a013e7dbc0e497c40cd0d0819
gyumong <rlaalsrb3559@naver.com>
2025-01-24 23:58:40 +0900

    Revert "fix: wrap in suspense"
    This reverts commit 2b024bc860de01fb17fd75ebe398a7d2caa07ab1.

--- app/(main)/academic-detail/layout.tsx
+++ app/(main)/academic-detail/layout.tsx
@@ -3,7 +3,6 @@
 import { useRouter } from 'next/navigation';
 import { TopNavigation } from '@/components/ui/TopNavigation';
 import styles from './layout.module.scss';
-import { Suspense } from 'react';
 
 export default function GraduationProgressLayout({ children }: { children: React.ReactNode }) {
   const router = useRouter();
@@ -11,9 +10,7 @@ export default function GraduationProgressLayout({ children }: { children: React
   return (
     <div className={styles.container}>
       <TopNavigation.Preset title="학기별 세부 성적" type="back" onNavigationClick={() => router.back()} />
-      <Suspense fallback={<div>로딩 중...</div>}>
-        <div className={styles.content}>{children}</div>
-      </Suspense>
+      <div className={styles.content}>{children}</div>
     </div>
   );
 }
