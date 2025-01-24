ba2904b94402c2dd953acd2fa228e42580df8299
gyumong <rlaalsrb3559@naver.com>
2025-01-24 15:03:55 +0900

    feat: 각종 icon 추가

--- components/ui/Icon/icons.ts
+++ components/ui/Icon/icons.ts
@@ -1,9 +1,29 @@
+import ArrowDownIcon from '@/assets/icons/arrow-down.svg';
 import ArrowLeftIcon from '@/assets/icons/arrow-left.svg';
 import ArrowRightIcon from '@/assets/icons/arrow-right.svg';
+import ArrowTopIcon from '@/assets/icons/arrow-top.svg';
 import CheckStatusOffIcon from '@/assets/icons/check-status-off.svg';
 import CheckStatusOnIcon from '@/assets/icons/check-status-on.svg';
 import CloseIcon from '@/assets/icons/close.svg';
 import DeleteIcon from '@/assets/icons/delete.svg';
+import InfoIcon from '@/assets/icons/info.svg';
+import KakaoIcon from '@/assets/icons/kakao.svg';
+import RefreshIcon from '@/assets/icons/refresh.svg';
 import SchoolIcon from '@/assets/icons/school.svg';
+import SettingIcon from '@/assets/icons/setting.svg';
 
-export { CloseIcon, DeleteIcon, SchoolIcon, CheckStatusOnIcon, CheckStatusOffIcon, ArrowRightIcon, ArrowLeftIcon };
+export {
+  ArrowTopIcon,
+  CloseIcon,
+  DeleteIcon,
+  SchoolIcon,
+  CheckStatusOnIcon,
+  CheckStatusOffIcon,
+  ArrowRightIcon,
+  ArrowLeftIcon,
+  ArrowDownIcon,
+  InfoIcon,
+  SettingIcon,
+  RefreshIcon,
+  KakaoIcon,
+};
