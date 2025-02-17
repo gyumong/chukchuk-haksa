734a8922d06a33c8d639bb55c7488d8d15afd437
gyumong <rlaalsrb3559@naver.com>
2025-02-17 18:42:59 +0900

    fix: main layout 수정

--- app/(main)/main/layout.tsx
+++ app/(main)/main/layout.tsx
@@ -1,11 +1,18 @@
+  'use client';
 import type { PropsWithChildren } from 'react';
 import { TopNavigation } from '@/components/ui/TopNavigation';
 import styles from './layout.module.scss';
+import { useRouter } from 'next/navigation';
 
 export default function FunnelLayout({ children }: PropsWithChildren) {
+  const router = useRouter();
+  const handleRightIconClick = () => {
+    router.push('/setting');
+  };
+
   return (
     <div className={styles.container}>
-      <TopNavigation.Preset title="수원대학교" type="none" rightIcon="setting" />
+      <TopNavigation.Preset title="수원대학교" type="none" rightIcon="setting" onRightIconClick={handleRightIconClick} />
       <div className={styles.content}>{children}</div>
     </div>
   );
