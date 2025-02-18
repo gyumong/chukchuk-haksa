afd606fc7d5aaac83cace94a15106cfc9f44cb60
gyumong <rlaalsrb3559@naver.com>
2025-02-18 20:43:57 +0900

    fix: landing layout에 kakao provider 씌우기

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,9 +1,13 @@
+import KakaoProvider from '@/components/KakaoProvider';
 import styles from './layout.module.scss';
 
 export default function LandingLayout({ children }: { children: React.ReactNode }) {
   return (
+    <KakaoProvider>
+
     <div className={styles.container}>
       <div className={styles.content}>{children}</div>
     </div>
+    </KakaoProvider>
   );
 }
