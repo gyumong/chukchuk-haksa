fb789a4513b31c8cbf1badb2a75cb8834820bee6
gyumong <rlaalsrb3559@naver.com>
2025-03-03 09:46:59 +0900

    test

--- app/api/suwon-scrape/start/route.ts
+++ app/api/suwon-scrape/start/route.ts
@@ -1,40 +1,45 @@
-// app/api/suwon-scrape/start/route.ts
-import { NextResponse } from 'next/server';
-import type { SupabaseClient } from '@supabase/supabase-js';
-import { getIronSession } from 'iron-session';
-import { v4 as uuidv4 } from 'uuid';
-import type { SessionData } from '@/lib/auth';
-import { sessionOptions } from '@/lib/auth';
-import { setTask } from '@/lib/crawling/scrape-task';
-import { createClient } from '@/lib/supabase/server';
-import { SyncAcademicRecordUseCase } from '@/server/application/academic-record/usecases/SyncAcademicRecordUseCase';
-import { InitializePortalConnectionUseCase } from '@/server/application/student/usecases/InitializePortalConnectionUseCase';
-import { PortalRepositoryImpl } from '@/server/infrastructure/portal/PortalRepositoryImpl';
-import { SupabaseAcademicRecordRepository } from '@/server/infrastructure/supabase/repository/SupabaseAcademicRecordRepository';
-import { SupabaseCourseOfferingRepository } from '@/server/infrastructure/supabase/repository/SupabaseCourseOfferingRepository';
-import { SupabaseCourseRepository } from '@/server/infrastructure/supabase/repository/SupabaseCourseRepository';
-import { SupabaseDepartmentRepository } from '@/server/infrastructure/supabase/repository/SupabaseDepartmentRepository';
-import { SupabaseProfessorRepository } from '@/server/infrastructure/supabase/repository/SupabaseProfessorRepository';
-import { SupabaseStudentCourseRepository } from '@/server/infrastructure/supabase/repository/SupabaseStudentCourseRepository';
-import { SupabaseUserRepository } from '@/server/infrastructure/supabase/repository/SupabaseUserRepository';
-import { SupabaseAuthService } from '@/server/infrastructure/supabase/SupabaseAuthService';
-import type { Database } from '@/types';
+  // app/api/suwon-scrape/start/route.ts
+  import { NextResponse } from 'next/server';
+  import type { SupabaseClient } from '@supabase/supabase-js';
+  import { getIronSession } from 'iron-session';
+  import { v4 as uuidv4 } from 'uuid';
+  import type { SessionData } from '@/lib/auth';
+  import { sessionOptions } from '@/lib/auth';
+  import { setTask } from '@/lib/crawling/scrape-task';
+  import { createClient } from '@/lib/supabase/server';
+  import { SyncAcademicRecordUseCase } from '@/server/application/academic-record/usecases/SyncAcademicRecordUseCase';
+  import { InitializePortalConnectionUseCase } from '@/server/application/student/usecases/InitializePortalConnectionUseCase';
+  import { PortalRepositoryImpl } from '@/server/infrastructure/portal/PortalRepositoryImpl';
+  import { SupabaseAcademicRecordRepository } from '@/server/infrastructure/supabase/repository/SupabaseAcademicRecordRepository';
+  import { SupabaseCourseOfferingRepository } from '@/server/infrastructure/supabase/repository/SupabaseCourseOfferingRepository';
+  import { SupabaseCourseRepository } from '@/server/infrastructure/supabase/repository/SupabaseCourseRepository';
+  import { SupabaseDepartmentRepository } from '@/server/infrastructure/supabase/repository/SupabaseDepartmentRepository';
+  import { SupabaseProfessorRepository } from '@/server/infrastructure/supabase/repository/SupabaseProfessorRepository';
+  import { SupabaseStudentCourseRepository } from '@/server/infrastructure/supabase/repository/SupabaseStudentCourseRepository';
+  import { SupabaseUserRepository } from '@/server/infrastructure/supabase/repository/SupabaseUserRepository';
+  import { SupabaseAuthService } from '@/server/infrastructure/supabase/SupabaseAuthService';
+  import type { Database } from '@/types';
 
-export async function POST(req: Request) {
-  const res = NextResponse.next();
-  const session = await getIronSession<SessionData>(req, res, sessionOptions);
-  const username = session.username;
-  const password = session.password;
+  export const config = {
+    runtime: 'edge',
+  };
 
-  if (!username || !password) {
-    return NextResponse.json({ error: '포털 로그인이 필요합니다.' }, { status: 401 });
-  }
-  let studentInfo;
-  const taskId = uuidv4();
-  setTask(taskId, 'in-progress', null);
+  export async function POST(req: Request, {waitUntil}: {waitUntil: (promise: Promise<void>) => void}) {
+    const res = NextResponse.next();
+    const session = await getIronSession<SessionData>(req, res, sessionOptions);
+    const username = session.username;
+    const password = session.password;
 
-  // 비동기로 크롤링 시작
-  // (async () => {
+    if (!username || !password) {
+      return NextResponse.json({ error: '포털 로그인이 필요합니다.' }, { status: 401 });
+    }
+    let studentInfo;
+    const taskId = uuidv4();
+    setTask(taskId, 'in-progress', null);
+
+    // 비동기로 크롤링 시작
+    waitUntil(
+    (async () => {
     try {
       const supabase: SupabaseClient<Database> = createClient();
       const portalRepository = new PortalRepositoryImpl();
@@ -46,17 +51,16 @@ export async function POST(req: Request) {
       const studentCourseRepository = new SupabaseStudentCourseRepository(supabase);
       const academicRecordRepository = new SupabaseAcademicRecordRepository(supabase);
       const courseOfferingRepository = new SupabaseCourseOfferingRepository(supabase);
-      console.log('fetch start')
+      console.log('fetch start');
       const portalData = await portalRepository.fetchPortalData(username, password);
       console.log('fetch finished');
-
+      console.log('portalData', portalData);
       const initializePortalConnectionUseCase = new InitializePortalConnectionUseCase(
         portalRepository,
         departmentRepository,
         userRepository,
         authService
       );
-
 
       const syncAcademicRecordUseCase = new SyncAcademicRecordUseCase(
         portalRepository,
@@ -73,13 +77,13 @@ export async function POST(req: Request) {
         throw new Error(initResult.error);
       }
       console.log('initializePortalConnectionUseCase Completed');
-      
+
       // (2) 학업 이력 동기화 유스케이스 실행
       const syncResult = await syncAcademicRecordUseCase.executeWithPortalData(portalData);
       if (!syncResult.isSuccess) {
         throw new Error(syncResult.error);
       }
-      
+
       console.log('syncAcademicRecordUseCase Completed');
 
       studentInfo = initResult.studentInfo;
@@ -87,11 +91,15 @@ export async function POST(req: Request) {
       setTask(taskId, 'completed', { message: '동기화 완료', studentInfo: initResult.studentInfo });
       // **세션 만료 처리**
       session.destroy(); // Iron Session에서 세션 데이터 삭제
-      console.log('Session destroyed after successful scrape');
+      console.log('Session destroyed after succes sful scrape');
+      
     } catch (err: any) {
-      setTask(taskId, 'failed', { message: err.message });
+      console.log('@error@', err.message, err);
+      // TODO cause 처리 로직 암묵적 의존성 내포 개선 필요
+      setTask(taskId, 'failed', { message: err.message, status: err.cause });
     }
-  // })();
+    })()
+  )
 
-  return NextResponse.json({ taskId, studentInfo }, { status: 202 });
-}
+    return NextResponse.json({ taskId, studentInfo }, { status: 202 });
+  }
