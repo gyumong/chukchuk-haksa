1fe35d37ecee05a21663ddc13f53948374c27115
gyumong <rlaalsrb3559@naver.com>
2025-03-06 23:12:28 +0900

    feat: 재연동 퍼널 추가

---
+++ app/(funnel)/resync-login/page.tsx
@@ -0,0 +1,85 @@
+'use client';
+
+import type { FormEvent } from 'react';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { captureException } from '@sentry/nextjs';
+import { FixedButton, TextField } from '@/components/ui';
+import { ROUTES } from '@/constants/routes';
+import { FunnelHeadline, SchoolCard } from '../components';
+import styles from './page.module.scss';
+
+export default function PortalLogin() {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const router = useRouter();
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    try {
+      setIsLoading(true);
+      setErrorMessage('');
+
+      const res = await fetch('/api/suwon-scrape/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password }),
+      });
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error(data.error || '로그인 실패');
+      }
+      router.push(`${ROUTES.FUNNEL.RESYNC_SCRAPING}`);
+    } catch (err: any) {
+      console.log(err);
+      captureException(err);
+      setErrorMessage(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <FunnelHeadline
+        title="재학 중인 학교<br/>계정을 연동해주세요"
+        description="척척학사에서 수집하는 개인 정보는<br/>학교 연동 후 즉시 폐기됩니다."
+      />
+
+      <form onSubmit={handleSubmit} className={styles.formContainer}>
+        <SchoolCard schoolName="수원대학교" />
+
+        <TextField
+          placeholder="학번을 입력해주세요"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+          error={Boolean(errorMessage)}
+        />
+
+        <TextField
+          type="password"
+          placeholder="비밀번호를 입력해주세요"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          error={Boolean(errorMessage)}
+        />
+
+        {errorMessage && (
+          <div className={styles.errorMessage}>
+            {errorMessage.split('\n').map((line, i) => (
+              <p key={i}>{line}</p>
+            ))}
+          </div>
+        )}
+
+        <FixedButton type="submit" disabled={!username || !password || isLoading} isLoading={isLoading}>
+          학업 이력 동기화하기
+        </FixedButton>
+      </form>
+    </div>
+  );
+}
---
+++ app/(funnel)/resync-scraping/error.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import * as Sentry from '@sentry/nextjs';
+import { FixedButton } from '@/components/ui';
+import ErrorScreen from '../components/ErrorScreen/ErrorScreen';
+import styles from './error.module.scss';
+import { ROUTES } from '@/constants/routes';
+
+const ScrapingErrorPage = ({ error }: { error: Error }) => {
+  // TODO 에러 정의 및 처리
+  useEffect(() => {
+    Sentry.captureException(error);
+    console.log('error Message', error.message);
+  }, [error]);
+  const router = useRouter();
+
+  const handleRetry = () => {
+    router.push(`${ROUTES.FUNNEL.RESYNC_LOGIN}`);
+  };
+  return (
+    <div className={styles.container}>
+      <ErrorScreen errorMessage={error.message} />
+      <FixedButton onClick={handleRetry}>다시 시도하기</FixedButton>
+    </div>
+  );
+};
+
+export default ScrapingErrorPage;
