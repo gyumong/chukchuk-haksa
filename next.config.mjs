a36827a3d099ee1751d19f574687f28037621008
gyumong <rlaalsrb3559@naver.com>
2025-04-02 18:27:30 +0900

    fix: next config alias 설정

--- next.config.mjs
+++ next.config.mjs
@@ -33,6 +33,8 @@ const nextConfig = {
     // Modify the file loader rule to ignore *.svg, since we have it handled now.
     fileLoaderRule.exclude = /\.svg$/i;
 
+    config.resolve.alias['@'] = path.resolve(__dirname);
+
     return config;
   },
 };
