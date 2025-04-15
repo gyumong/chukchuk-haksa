eed8a9be1f23b2937d3e5e3a1f89aacf91093af9
gyumong <rlaalsrb3559@naver.com>
2025-04-15 12:43:46 +0900

    feat: font preload 처리

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,6 +58,11 @@ const suit = localFont({
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
+      <head>
+        <link rel="preload" href="/fonts/Paperlogy-subset-7Bold.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="/fonts/SUIT-subset-Regular.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="/fonts/SUIT-subset-SemiBold.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+      </head>
       <body className={`${paperlogy.variable} ${suit.variable} antialiased`}>
         {children}
         <Analytics />
