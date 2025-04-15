29ef1b48158c5258d7800d0a4e0fd4eeaf604ed8
gyumong <60845910+gyumong@users.noreply.github.com>
2025-04-15 16:03:24 +0900

    [feat]: next image 캐싱 처리 및 formats avif 우선 (#119)
    * feat: next image 캐싱 처리 및 formats avif 우선
    
    * ci: apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,19 @@
+import withBundleAnalyzerFn from '@next/bundle-analyzer';
 import { withSentryConfig } from '@sentry/nextjs';
 import path from 'path';
 
 /** @type {import('next').NextConfig} */
 
+const withBundleAnalyzer = withBundleAnalyzerFn({
+  enabled: process.env.NODE_ENV === 'development',
+});
+
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 60 * 60 * 24 * 365, // 1년
+  },
   sassOptions: {
     // import.meta.dirname는 node v20.11 이상 부터 가능
     // @example https://nodejs.org/api/esm.html#importmetadirname
@@ -39,47 +48,49 @@ const nextConfig = {
   },
 };
 
-export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
+export default withBundleAnalyzer(
+  withSentryConfig(nextConfig, {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
 
-  org: 'student-xhx',
-  project: 'cchaksa',
-  sourcemaps: {
-    deleteSourcemapsAfterUpload: true, // Source Map 업로드 후 삭제
-  },
+    org: 'student-xhx',
+    project: 'cchaksa',
+    sourcemaps: {
+      deleteSourcemapsAfterUpload: true, // Source Map 업로드 후 삭제
+    },
 
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
-  // An auth token is required for uploading source maps.
-  authToken: process.env.SENTRY_AUTH_TOKEN,
+    // Only print logs for uploading source maps in CI
+    silent: !process.env.CI,
+    // An auth token is required for uploading source maps.
+    authToken: process.env.SENTRY_AUTH_TOKEN,
 
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
 
-  // Automatically annotate React components to show their full name in breadcrumbs and session replay
-  reactComponentAnnotation: {
-    enabled: true,
-  },
+    // Automatically annotate React components to show their full name in breadcrumbs and session replay
+    reactComponentAnnotation: {
+      enabled: true,
+    },
 
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  tunnelRoute: '/monitoring',
+    // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+    // This can increase your server load as well as your hosting bill.
+    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+    // side errors will fail.
+    tunnelRoute: '/monitoring',
 
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
 
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
 
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
-});
+    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true,
+  })
+);
