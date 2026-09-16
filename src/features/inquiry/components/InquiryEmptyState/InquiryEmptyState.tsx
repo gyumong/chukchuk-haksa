a6201595d44018648d1a9a6caa57c8769c162ce6
JJ-Gun <132339110+JJ-Gun@users.noreply.github.com>
2026-09-17 06:15:53 +0900

    fix: emptyState 수정

--- src/features/inquiry/components/InquiryEmptyState/InquiryEmptyState.tsx
+++ src/features/inquiry/components/InquiryEmptyState/InquiryEmptyState.tsx
@@ -1,11 +1,8 @@
-import Image from 'next/image';
 import styles from './InquiryEmptyState.module.scss';
 
 export function InquiryEmptyState() {
   return (
     <div className={styles.container}>
-      {/* TODO: 디자이너에게 마스코트 에셋 받아서 교체 */}
-      <Image src="/images/illustrations/InquiryEmpty.png" alt="" width={64} height={64} />
       <p className={styles.text}>문의 내역이 존재하지 않습니다.</p>
     </div>
   );
